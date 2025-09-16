1957a1c269140bfae1902ea2ee3585751f725387
Anurag Majumdar <anusom2002@gmail.com>
2025-09-16 21:19:13 +0530

    Salary Filter

--- backend/job/filters.py
+++ backend/job/filters.py
@@ -2,6 +2,10 @@ from django_filters import rest_framework as filters
 from .models import Job
 
 class JobsFilters(filters.FilterSet):
+
+    min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr='gte')
+    max_salary = filters.NumberFilter(field_name="salary" or 10000000, lookup_expr='lte')
+
     class Meta:
         model = Job
-        fields = ('Education', 'jobType', 'Experience')
+        fields = ('Education', 'jobType', 'Experience', 'min_salary', 'max_salary')
