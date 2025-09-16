4ef838c0a4af617681f6a1670f55d0f34b202239
Anurag Majumdar <anusom2002@gmail.com>
2025-09-16 21:26:09 +0530

    Location and Keyword Filter

--- backend/job/filters.py
+++ backend/job/filters.py
@@ -3,6 +3,8 @@ from .models import Job
 
 class JobsFilters(filters.FilterSet):
 
+    keyword = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    location = filters.CharFilter(field_name='address', lookup_expr='icontains')
     min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr='gte')
     max_salary = filters.NumberFilter(field_name="salary" or 10000000, lookup_expr='lte')
 
